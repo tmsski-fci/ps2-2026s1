a850b9b05dc72b977ec13c39880ffda943096ed2
tmsski <20782956+tmsski@users.noreply.github.com>
2026-04-16 01:48:01 +0000

    restdb titulos

--- restdb/src/main/java/ps2/restdb/TitulosController.java
+++ restdb/src/main/java/ps2/restdb/TitulosController.java
@@ -1,8 +1,13 @@
 package ps2.restdb;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +26,21 @@ public class TitulosController {
     @PostMapping("/api/titulos")
     public Titulo createTitulo(@RequestBody Titulo t) {
         return titulosRepo.save(t);
+    }
+
+    @PutMapping("/api/titulos/{id}")
+    public Titulo updateTitulo(@PathVariable long id, @RequestBody Titulo t) {
+        Optional<Titulo> opt = titulosRepo.findById(id);
+        if (opt.isEmpty()) return null;
+        Titulo titulo = opt.get();
+        titulo.setNome(t.getNome());
+        titulo.setEsporte(t.getEsporte());
+        titulo.setPremio(t.getPremio());
+        return titulosRepo.save(titulo);
+    }
+
+    @DeleteMapping("/api/titulos/{id}")
+    public void deleteTitulo(@PathVariable long id) {
+        titulosRepo.deleteById(id);
     }
 }
